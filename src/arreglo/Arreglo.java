6e4949897d6c960e5d3ed06a2638e5df90ed2861
TIC <TIC@192.168.0.7>
2024-02-29 11:24:49 -0500

    termino 😁

--- src/arreglo/Arreglo.java
+++ src/arreglo/Arreglo.java
@@ -20,7 +20,7 @@ public class Arreglo {
         static double precioV [] =  {0.20,0.50,1.00,2.50,0.50,3.00};
         static String lacteos [] = {"leche","yougur","mantequilla","queso freco"," queso parmesano","queso mosarela"};
         static double precioL [] =  {1.50,2.50,3.00,2.50,0.50,3.00};
-        static double total = 0; static int cantidad, cantidadElegida = 0; 
+        static double total = 0; static int cantidad, cantidadElegida = 0,totalFinal, cantidadFinal; 
         static Scanner input = new Scanner(System.in);
         static int opcion = 0,elecionSec,Neejir = 0;
     public static void main(String[] args) {
@@ -31,21 +31,22 @@ public class Arreglo {
             System.out.println("si(9) no(12)");
             opcion =  input.nextInt();
         }            
-        System.out.println("total a pagar: " + total + " cantida de productos" + cantidadElegida);
+        System.out.println("total a pagar: " + totalFinal + " cantidad de productos " + cantidadFinal);
     }
     //funciones
     public void menuInicio(){
         System.out.println("elija la section:");
+        Neejir = 0;
         for(String set : section){
             System.out.println("("+ Neejir +")" + set);
-            if(Neejir < section.length){
+            if(Neejir <= section.length){
                 Neejir++;
             }else{
                 Neejir = 0;
             }
             
         }
-        System.out.println("escoger preoducto");
+        System.out.println("[escoger preoducto]");
         elecionSec =  input.nextInt();
         switch (section[elecionSec]) {
             case "abarrotes" -> {
@@ -67,12 +68,12 @@ public class Arreglo {
     
     public void getAbarrotes(){
                 System.out.println("section " + section[elecionSec]);
-                System.out.println("elegir producto");
+                System.out.println("[elegir producto]");
                 while(opcion != 10){
                     Neejir = 0;
                     for(String a : abarrotes){
                         System.out.println("("+ Neejir +")" + a);
-                        if(Neejir < lacteos.length){
+                        if(Neejir <= lacteos.length){
                             Neejir++;
                         }else{
                             Neejir = 0;
@@ -80,13 +81,15 @@ public class Arreglo {
                     }
                     elecionSec =  input.nextInt();
                     System.out.println("producto selecionado " + abarrotes[elecionSec] + " precio " +precioA[elecionSec]);
-                    System.out.println("canidad del producto");
+                    System.out.println("canidad del producto ?");
                     cantidad = input.nextInt();
                     total += precioA[elecionSec]*cantidad;
+                    totalFinal+= total;
                     cantidadElegida++;
-                    System.out.println("Monto a pagar " + total);
-                    System.out.println("producto escogido" + cantidadElegida);
-                    System.out.println("desea compra mas producto");
+                    cantidadFinal+=cantidadElegida;
+                    System.out.println("Monto a pagar: " + total);
+                    System.out.println("cantidad de productos: " + cantidadElegida);
+                    System.out.println("desea algo mas?");
                     System.out.println("si(9)  no (10)");
                     opcion = input.nextInt();
                 }
@@ -94,12 +97,12 @@ public class Arreglo {
     }
     public void getPanaderia(){
                 System.out.println("section " + section[elecionSec]);
-                System.out.println("elegir producto");
+                System.out.println("elegir producto?");
                 while(opcion != 10){
                     Neejir = 0;
                     for(String a : panaderia){
                         System.out.println("("+ Neejir +")" + a);
-                        if(Neejir < lacteos.length){
+                        if(Neejir <= lacteos.length){
                             Neejir++;
                         }else{
                             Neejir = 0;
@@ -107,25 +110,27 @@ public class Arreglo {
                     }
                     elecionSec =  input.nextInt();
                     System.out.println("producto selecionado " + panaderia[elecionSec] + " precio " +precioP[elecionSec]);
-                    System.out.println("canidad del producto");
+                    System.out.println("canidad del producto?");
                     cantidad = input.nextInt();
                     total += precioP[elecionSec]*cantidad;
+                    totalFinal+= total;
                     cantidadElegida++;
-                    System.out.println("Monto a pagar " + total);
-                    System.out.println("producto escogido" + cantidadElegida);
-                    System.out.println("desea compra mas producto");
+                    cantidadFinal+=cantidadElegida;
+                    System.out.println("Monto a pagar: " + total);
+                    System.out.println("catidad de producto: " + cantidadElegida);
+                    System.out.println("desea algo mas?");
                     System.out.println("si(9)  no (10)");
                     opcion = input.nextInt();
                 }
     }
     public void getVerdura(){
                 System.out.println("section " + section[elecionSec]);
-                System.out.println("elegir producto");
+                System.out.println("elegir producto?");
                 while(opcion != 10){
                     Neejir = 0;
                     for(String a : verdura){
                         System.out.println("("+ Neejir +")" + a);
-                        if(Neejir < lacteos.length){
+                        if(Neejir <= lacteos.length){
                             Neejir++;
                         }else{
                             Neejir = 0;
@@ -136,10 +141,12 @@ public class Arreglo {
                     System.out.println("canidad del producto");
                     cantidad = input.nextInt();
                     total += precioA[elecionSec]*cantidad;
+                    totalFinal+= total;
                     cantidadElegida++;
-                    System.out.println("Monto a pagar " + total);
-                    System.out.println("producto escogido" + cantidadElegida);
-                    System.out.println("desea compra mas producto");
+                    cantidadFinal+=cantidadElegida;
+                    System.out.println("Monto a pagar: " + total);
+                    System.out.println("cantidad de productos: " + cantidadElegida);
+                    System.out.println("desea algo mas?");
                     System.out.println("si(9)  no (10)");
                     opcion = input.nextInt();
                 }
@@ -151,7 +158,7 @@ public class Arreglo {
                     Neejir = 0;
                     for(String a : lacteos){
                         System.out.println("("+ Neejir +")" + a);
-                        if(Neejir < lacteos.length){
+                        if(Neejir <= lacteos.length){
                             Neejir++;
                         }else{
                             Neejir = 0;
@@ -162,10 +169,12 @@ public class Arreglo {
                     System.out.println("canidad del producto");
                     cantidad = input.nextInt();
                     total += precioA[elecionSec]*cantidad;
+                    totalFinal+= total;
                     cantidadElegida++;
-                    System.out.println("Monto a pagar " + total);
-                    System.out.println("producto escogido" + cantidadElegida);
-                    System.out.println("desea compra mas producto");
+                    cantidadFinal+=cantidadElegida;
+                    System.out.println("Monto a pagar: " + total);
+                    System.out.println("cantidad de productos: " + cantidadElegida);
+                    System.out.println("desea algo mas?");
                     System.out.println("si(9)  no (10)");
                     opcion = input.nextInt();
                 }
