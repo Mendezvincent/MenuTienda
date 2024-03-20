1aaf218c3971e8be5a9e5e6215b209335f40677d
vincent mendoza <gamecro971@gmail.com>
2024-03-20 13:58:24 -0500

    por terminar

--- src/po02/cliente.java
+++ src/po02/cliente.java
@@ -1,41 +1,89 @@
 package po02;
 
 import java.util.Scanner;
-
 /**
  *
  * @author LAB02-PC02
  */
 public class cliente {
     private int password[] = new int[4];
+    private int id , opcion = 0;
+    private double saldo[] = new double[4];
     private String nombre[] =  new  String[4];
     private Scanner input = new Scanner(System.in);
     public void registra(){
         System.out.println("REGISTRO DE CUENTAS");
-        for(int i =  0; i <  4 ; i++){
+        for(int i =  0; i <  nombre.length ; i++){
         System.out.print("user: ");
         nombre[i] = input.next();
-        System.out.print("\npassword: ");
+        System.out.print("password: ");
         password[i] =  input.nextInt();
+        System.out.println("");
+        saldo[i] =  0.0;
         }
     }
     
-    public void  validarDate(){
-        System.out.println("sesion bco");
+    public boolean validarDate(){
+        boolean psar = false;
+        System.out.println("\t***************");
+        System.out.println("\t** BANCO BCV **");
+        System.out.println("\t***************");
         System.out.print("user: ");
         String n =  input.next();
-        System.out.print("\npassword: ");
+        System.out.print("password: ");
         int pass = input.nextInt();
         for(int a = 0 ; a  < 4 ; a++){
             if(n.equals(nombre[a])  &&  pass == password[a]){
                 System.out.println("sesion exitosa");
+                id =  a;
+                psar =  true;
                 break;
             }else{
                 System.out.println("user o password no validos");
+                psar =  false;
                 break;
             }
         }
+        return psar;
     }
     
+    public void menu(){
+        System.out.println("hola " + nombre[id]);
+        System.out.println("*********************************************");
+        System.out.println("** saldo disponible:  s/." + saldo[id] +"  **");
+        System.out.println("*********************************************");
+    }
+    
+    public void depositar(){
+        System.out.print("monto a depositar: ");
+        double monto =  input.nextDouble();
+        saldo[id] += monto;
+        System.out.println("*********************************************");
+        System.out.println("** saldo disponible:  s/." + saldo[id] +"  **");
+        System.out.println("*********************************************");
+    }
+    public void trafrerir(){
+        System.out.println("depositar");
+        System.out.println("lista");
+        int d = 0;
+        for(String name : nombre){
+            System.out.println("id("+ d +") user: "+name);
+            d++;
+        }
+        System.out.print("\ningrese el id para depostar:");
+        opcion = input.nextInt();
+        System.out.print("\nmonto del deposito: ");
+        double monto = input.nextDouble();
+        if(saldo[id] > 0.0){
+            saldo[id] -=monto;
+            saldo[opcion] = monto;
+            System.out.println("el deposito fue un exito");
+            System.out.println("*********************************************");
+            System.out.println("** saldo disponible:  s/." + saldo[id] +"  **");
+            System.out.println("*********************************************");
+        }else{
+            System.out.println("fondo insuficiente");
+        }
+    }
     
 }
--- src/po02/main.java
+++ src/po02/main.java
@@ -1,8 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package po02;
+
+import java.util.Scanner;
 
 /**
  *
@@ -10,9 +8,39 @@ package po02;
  */
 public class main {
     public static void main(String[] args) {
-        cliente juan = new cliente();
-        juan.registra();
-        juan.validarDate();
-        
+        int opcion = 1;
+        String op;
+        Scanner a = new Scanner(System.in);
+        cliente clint = new cliente();
+            System.out.println("\t***************");
+            System.out.println("\t** BANCO BCV **");
+            System.out.println("\t***************");
+            while(opcion != 0){
+            System.out.println("que desea hacer?");
+            System.out.println("registra");
+            System.out.println("inicia sesion");
+            op = a.next();
+            switch (op) {
+                case "registra" -> clint.registra();
+                case "inicia sesion" -> {
+                    if(clint.validarDate()){
+                        clint.menu();
+                        while(opcion != 0){
+                            System.out.println("depositar");
+                            System.out.println("trasferir");
+                            op =  a.next();
+                            if(op.equals("depositar")){
+                                clint.depositar();
+                            }else if(op.equals("trasferir")){
+                                clint.trafrerir();
+                            }
+                        }
+                    }else{
+                        System.out.println("error por bot v:");
+                    }
+                }
+                default -> System.out.println("opcion no validad");
+            }
+        }
     }
 }
